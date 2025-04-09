eca5ed720820a4217c4a89a0241e47b2f627bdc9
dayrui <dayrui@gmail.com>
2025-04-10 00:05:40 +0800

    9999

---
+++ dayrui/My/Config/License.php
@@ -0,0 +1,10 @@
+<?php
+// 此文件是版本文件，每次下载安装包会自动生成，请勿修改
+return [
+
+    'license' => 'dev',
+    'oem' => 'RuiXunCMS',
+    'name' => 'RuiXunCMS',
+    'url' => 'http://www.ruixuncms.com',
+
+];
---
+++ dayrui/My/Config/Version.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'id' => '0',
+    'name' => 'RuiXunCMS',
+    'version' => '4.6.2',
+    'updatetime' => '-',
+    'downtime' => '-',
+
+];
